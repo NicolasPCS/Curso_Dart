266db2e433640a1fb7217a7072959cd2e1135a57
Nicolas Caytuiro <nicolascaytuirosilva@gmail.com>
2020-12-07 19:03:55 -0500

    Rutas generadas automaticamente y archivo de rutas independiente y Card widgets

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
- 
+
 // import 'package:componentes/src/pages/home_temp.dart';
-import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,10 +14,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/' : (BuildContext context) => HomePage(),
-        'alert' : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      onGenerateRoute: (RouteSettings settings) {
+        print('Ruta llamada: ${settings.name}');
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
